ded6c804cb21076a34d2f4fcf5f1fc9e735b645c
catffeiner <010076895@student.iga.edu>
2023-09-29 15:34:17 -0600

    hfhfghf

--- Proyecto Municode/scrippt.js
+++ Proyecto Municode/scrippt.js
@@ -1,20 +1,19 @@
-// Obtener elementos del DOM comunes a todas las páginas
 const content = document.getElementById("content");
-
 let sitiosTuristicos = [];
 
-// Función para mostrar la página de registro de sitios
+
 function mostrarRegistroSitio() {
-    content.innerHTML = ""; // Limpiar contenido existente
+    content.innerHTML = ""; 
     const registroForm = document.createElement("form");
-    registroForm.innerHTML = `
-        <h2>Sitios Turísticos</h2>
+    registroForm.innerHTML = 
+    `<h2>Sitios Turísticos</h2>
         <label for="pais">País:</label>
         <select id="pais" name="pais">
             <option value="Guatemala">Guatemala</option>
             <option value="Honduras">Honduras</option>
             <option value="El Salvador">El Salvador</option>
-            <!-- Agrega más países aquí -->
+            <option value="Belice">Belice</option>
+            <option value="Costa Rica">Costa Rica</option>
         </select>
         <label for="nombre">Nombre:</label>
         <input type="text" id="nombre" name="nombre" required>
@@ -22,39 +21,39 @@ function mostrarRegistroSitio() {
         <input type="text" id="horario" name="horario">
         <label for="descripcion">Descripción:</label>
         <textarea id="descripcion" name="descripcion" required></textarea>
-        <button type="button" id="registrar-sitio">Registrar</button>
-    `;
+        <button type="button" id="registrar-sitio">Registrar</button>`;
     content.appendChild(registroForm);
 
     const registrarSitioBtn = document.getElementById("registrar-sitio");
     registrarSitioBtn.addEventListener("click", () => {
-        // Obtener valores del formulario
+
         const pais = document.getElementById("pais").value;
         const nombre = document.getElementById("nombre").value;
         const horario = document.getElementById("horario").value;
         const descripcion = document.getElementById("descripcion").value;
 
-        // Validar y guardar el sitio turístico
         if (nombre && descripcion) {
             const sitio = { pais, nombre, horario, descripcion, likes: 0 };
             sitiosTuristicos.push(sitio);
             alert("Sitio turístico registrado con éxito.");
+            localStorage.setItem("sitiosTuristicos",JSON.stringify(sitiosTuristicos));
+            window.location.href ="mostrar.html"
         } else {
             alert("Por favor, complete los campos obligatorios.");
         }
     });
 }
 
-// Función para mostrar la página de sitios turísticos
+
+
 function mostrarSitiosTuristicos() {
-    content.innerHTML = ""; // Limpiar contenido existente
+    content.innerHTML = ""; 
     const listaSitios = document.createElement("div");
     listaSitios.innerHTML = "<h2>Lista de Sitios Turísticos</h2>";
 
-    // Crear una tabla para mostrar los sitios
     const tabla = document.createElement("table");
-    tabla.innerHTML = `
-        <thead>
+    tabla.innerHTML = 
+    `<thead>
             <tr>
                 <th>País</th>
                 <th>Nombre</th>
@@ -63,43 +62,34 @@ function mostrarSitiosTuristicos() {
             </tr>
         </thead>
         <tbody>
-        </tbody>
-    `;
+        </tbody>`;
 
     // Llenar la tabla con los datos de los sitios turísticos
     const tbody = tabla.querySelector("tbody");
     sitiosTuristicos.forEach((sitio, index) => {
         const row = document.createElement("tr");
-        row.innerHTML = `
-            <td>${sitio.pais}</td>
-            <td>${sitio.nombre}</td>
+        row.innerHTML = 
+        `<td>${sitio.pais}</td>
+        <td>${sitio.nombre}</td>
             <td>
                 <button id="ver-btn-${index}">Ver</button>
                 <button id="modificar-btn-${index}" ${sitio.likes > 0 ? 'disabled' : ''}>Modificar</button>
                 <button id="eliminar-btn-${index}" ${sitio.likes > 0 ? 'disabled' : ''}>Eliminar</button>
             </td>
-            <td>${sitio.likes}</td>
-        `;
+            <td>${sitio.likes}</td>`;
 
-        // Manejar clic en el botón "Ver"
         const verBtn = row.querySelector(`#ver-btn-${index}`);
         verBtn.addEventListener("click", () => {
-            // Aquí puedes implementar la acción para ver los detalles del sitio
-            // Por ejemplo, mostrar la descripción y otros datos en un modal.
-        });
+         });
 
         // Manejar clic en el botón "Modificar"
         const modificarBtn = row.querySelector(`#modificar-btn-${index}`);
         modificarBtn.addEventListener("click", () => {
-            // Aquí puedes implementar la acción para modificar el sitio
-            // Si el sitio tiene likes, la modificación estará deshabilitada.
         });
 
         // Manejar clic en el botón "Eliminar"
         const eliminarBtn = row.querySelector(`#eliminar-btn-${index}`);
         eliminarBtn.addEventListener("click", () => {
-            // Aquí puedes implementar la acción para eliminar el sitio
-            // Si el sitio tiene likes, la eliminación estará deshabilitada.
         });
 
         tbody.appendChild(row);
@@ -112,10 +102,8 @@ const currentLocation = window.location.pathname;
 if (currentLocation.endsWith("registro.html")) {
     mostrarRegistroSitio();
 } else if (currentLocation.endsWith("mostrar.html")) {
-    mostrarSitiosTuristicos();
-}
-const storedData = localStorage.getItem("sitiosTuristicos");
+    const storedData = localStorage.getItem("sitiosTuristicos");
 if (storedData) {
     sitiosTuristicos = JSON.parse(storedData);
+}     mostrarSitiosTuristicos();
 }
-
